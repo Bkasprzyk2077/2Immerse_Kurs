77279d25943fb35bfa85fa5d31f0323377e4cefa
Bkasprzyk2077 <dolarbartek@wp.pl>
2025-03-23 16:12:02 +0100

    player

---
+++ 2immerse_kurs/scenes/player/player.gd
@@ -0,0 +1,52 @@
+extends CharacterBody2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
+
+var is_attacking: bool = false
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("attack") and is_on_floor():
+		is_attacking = true
+		animated_sprite_2d.play("attack")
+		await animated_sprite_2d.animation_finished
+		is_attacking = false
+		
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction and !is_attacking:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	animate()
+	move_and_slide()
+
+func animate():
+	if velocity.x > 0:
+		animated_sprite_2d.flip_h = false
+	elif velocity.x < 0:
+		animated_sprite_2d.flip_h = true
+		
+	if !is_on_floor():
+		animated_sprite_2d.play("in_air")
+		return
+		
+	if is_attacking: return
+		
+	if velocity != Vector2.ZERO:
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+		
